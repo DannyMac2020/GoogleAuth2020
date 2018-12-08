cd00e57927b6e56f59bd7caba1df280ad91e395c
Daniel McIntyre <daniel-mcintyre@hotmail.com>
2018-12-08 10:21:57 -0800

    first push

---
+++ client/src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
